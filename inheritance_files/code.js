2c3d9f71a2f55f3f5b58ab2a0ec2f88d3581bbcc
Mohammed Fahim Uddin <fahim.uddin@comparcelub.com.au>
2022-11-14 07:39:13 -0600

    inheritance

---
+++ inheritance_files/code.js
@@ -0,0 +1,11 @@
+console.log(filterString("asd asd asd ",["asd","asdad"]));
+function filterString(str,rmv){
+    let r;
+result = rmv.forEach(function(x){
+
+    r = str.replace('/asd/','ff');
+    console.log(r);
+});
+    r = str.replace('/asd/','ff');
+return r;
+}
